a9268ca1c45967accc3b37aaefa04328c6f65c8e
nikita-baliassniy <Mefistoufel13>
2022-05-30 13:55:49 +0300

    Добавление артефакта

--- iBankTest/src/main/java/ru/netology/fields/TextInput.java
+++ iBankTest/src/main/java/ru/netology/fields/TextInput.java
@@ -27,14 +27,15 @@ public class TextInput extends AbstractField {
 	@Override
 	public void type(String value) {
 		SelenideElement element = this.element;
+		int counter = 15;
 		element.waitUntil(Condition.enabled, DriverManager.getDefaultImplicitlyWait());
-		while (!element.getValue().equals(value) && !element.getValue().replaceAll(" ", "")
-				.equals(value)) {
+		while (!element.getValue().equals(value) && counter > 0) {
 			try {
 				Thread.sleep(200);
 				element.sendKeys(Keys.chord(Keys.CONTROL, "a"));
 				element.sendKeys(Keys.BACK_SPACE);
 				element.sendKeys(value);
+				counter++;
 			} catch (InterruptedException ex) {
 				ex.printStackTrace();
 			}
